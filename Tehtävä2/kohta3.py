7aaca4341d916ae136f321cb1ee4ce56d43ca483
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-21 12:16:29 +0300

    Tehtävä 3 tehty

---
+++ Tehtävä2/kohta3.py
@@ -0,0 +1,8 @@
+Kanta = float(input("Kanta:"))
+Korkeus = float(input("Korkeus:"))
+
+Piiri= (Kanta * 2) + (Korkeus * 2)
+Pinta = Kanta * Korkeus
+float(Pinta)
+float(Piiri)
+print("Suorakulmion piiri olisi:",Piiri, "Ja pinta-ala:",Pinta)
