c39becd6092fce3a545f248eedd394fefdd09f5b
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-21 13:02:40 +0300

    Tehtävä 4 tehty

---
+++ Tehtävä2/kohta4.py
@@ -0,0 +1,11 @@
+numero1 = float(input("Syötä ensimmäinen numero: "))
+numero2 = float(input("Syötä toinen numero: "))
+numero3 = float(input("Syötä kolmas numero: "))
+
+Summa = numero1 + numero2 + numero3
+Tulo = numero1 * numero2 * numero3
+Keskiarvo = Summa / 3
+
+print("Numeroiden summa:",Summa)
+print("Numeroiden tulo:",Tulo)
+print("Numeroiden keskiarvo:",Keskiarvo)
