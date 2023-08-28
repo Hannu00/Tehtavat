78c88faea288c06d7f62407f8dd814804d7a933f
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-28 18:18:06 +0300

    tehtävä 6 kohta 6 tehty

---
+++ Tehtävä6/kohta6.py
@@ -0,0 +1,20 @@
+import math
+
+pizza_h1 = float(input("Syötä pizzan1 halkaisija senttimetreinä: "))
+pizza_e1 = float(input("Syötä pizzan1 hinta euroina: "))
+
+pizza_h2 = float(input("Syötä pizzan2 halkaisija senttimetreinä: "))
+pizza_e2 = float(input("Syötä pizzan2 hinta euroina: "))
+
+def pizzuri(halkaisija,eurot):
+    a = math.pi * halkaisija *halkaisija
+    b = a / eurot
+    return b
+
+pizza1 = pizzuri(pizza_h1,pizza_e1)
+pizza2 = pizzuri(pizza_h2,pizza_e2)
+
+if pizza1 > pizza2:
+    print(f"Pizza1 on parempi kuin pizza2")
+else:
+    print(f"Pizza2 on parempi kuin pizza1")
