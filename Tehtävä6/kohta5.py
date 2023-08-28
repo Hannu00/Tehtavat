1bc4875a97cae744fb442a8ae3d4dccc3b156961
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-28 17:55:43 +0300

    tehtävä 6 kohta 5 tehty

--- Tehtävä6/kohta5.py
+++ Tehtävä6/kohta5.py
@@ -1,18 +1,18 @@
 import random
-
+#luodaan ensimmäinen lista johonka käyttäjä syöttää koon
 random.seed(0)
-l_koko = int(input("Syötä listan koko"))
+l_koko = int(input("Syötä listan koko: "))
 k = [random.randint(1, 999) for i in range(l_koko)]
-
+#luodaan toinen lista nimelät k2
 k2 = []
-
-
-def karsija(k):
-    for i in range(l_koko):
-        if k[i] % 2 == 0:
-            print("just")
-        else:
-            k2.append(k[i])
-
-
-print(k, "\n", k2)
+#funktio joka lisää alkuperäisen listan numeroista parilliset uuteen listaan
+def karsija(lista, lista2):
+    for i in range(len(lista)):
+        vali = int(lista[i])
+        if vali % 2 == 0:
+            lista2.append(vali)
+    return
+#kutsutaan funktio
+karsija(k,k2)
+#tulostetaan molemmat listat
+print(f"{k}\n{k2}")
