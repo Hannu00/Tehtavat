2d7589e63ad264d70fe163d381d10a992d06b1e7
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-28 10:26:15 +0300

    Tehtävä 3 kohta 2 päivitetty lukemaan input myös pienellä

--- tehtavat3/kohta2.py
+++ tehtavat3/kohta2.py
@@ -5,13 +5,13 @@ A_hytti = "A on ikkunallinen hytti autokannen yläpuolella."
 B_hytti = "B on ikkunaton hytti autokannen yläpuolella."
 C_hytti = "C on ikkunaton hytti autokannen alapuolella."
 
-if hytti=="LUX":
+if hytti=="LUX" or hytti=="lux" or hytti=="Lux":
     print(LUX_hytti)
-elif hytti=="A":
+elif hytti=="A" or hytti=="a":
     print(A_hytti)
-elif hytti=="B":
+elif hytti=="B" or hytti=="b":
     print(B_hytti)
-elif hytti=="C":
+elif hytti=="C" or hytti=="c":
     print(C_hytti)
 else:
     print("Tarkista syötteesi!")
