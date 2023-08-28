ffe3aa6be1475da429ead2f48bfe8eafb6e0fc24
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-28 10:38:39 +0300

    Tehtävä 4 kohta 6 tehty

--- tehtavat3/kohta1.py
+++ tehtavat3/kohta1.py
@@ -1,7 +1,8 @@
 kuha = float(input("Syötä kuhan pituus senttimetreinä: "))
 mitta = float(37)
+tulos = mitta - kuha
 
 if kuha<mitta:
-    print("Laske kuha takaisin!\n","Alimmasta sallitusta pyyntimitasta puuttuu: ", mitta - kuha,"cm")
+    print("Laske kuha takaisin!\n",f"Alimmasta sallitusta pyyntimitasta puuttuu: {tulos:.2f}cm")
 else:
     print("Hieno saalis!")
