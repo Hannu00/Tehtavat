84992031bda86ad3920635479697d31462404d4f
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-29 16:20:19 +0300

    tehtävä 7 kohta 1 lisätty kommentti

--- Tehtävä7/kohta1.py
+++ Tehtävä7/kohta1.py
@@ -2,3 +2,5 @@ vuodenajat = { 1: "talvi", 2: "talvi", 3: "kevät", 4: "kevät", 5: "kevät", 6:
 kuukausi = int(input("Syötä kuukausi väliltä 1-12: "))
 
 print(f"Kuukauden {kuukausi} vuoden aika on {vuodenajat[kuukausi]}.")
+
+#toinen vaihtoehto olisi ollut käyttää vuodenajat = "talvi", "talvi", "talvi", "kevät"..... en tiedä kumpaa tässä haluttiin käytettävän joten valitsin omasta mielestäni helpomman tavan.
