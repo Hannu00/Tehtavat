433adb0f2b09c2480d5541225b5ac13a64bdab80
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-08-29 22:05:07 +0300

    tehtävä 7 kohta 3 versio 2. Vaatii tiedoston lentokentat.txt

--- Tehtävä7/kohta3.py
+++ Tehtävä7/kohta3.py
@@ -1,10 +1,11 @@
 
 
-def vihrea(teksti): print("\033[92m {}\033[00m" .format(teksti))
+def vihrea(teksti):
+    return "\033[92m {}\033[00m" .format(teksti)
 
 
-
-def punainen(teksti): print("\033[91m {}\033[00m" .format(teksti))
+def punainen(teksti):
+    return "\033[91m {}\033[00m" .format(teksti)
 
 
 def kirjoita(uusi):
@@ -14,21 +15,33 @@ def kirjoita(uusi):
 
 
 def etsi_t(icao):
-    with open(r"lentokentat.txt", "r") as fp:
+    with open("lentokentat.txt", "r", encoding="utf-8") as fp:
         for rivi_n, rivi in enumerate(fp):
             if icao in rivi:
                 return rivi
+        return punainen("Ei löytynyt")
 
 
 while True:
     syote = input(f'Kirjoita "lisaa" jos haluat lisätä tietoa, "etsi" jos haluat etsiä tietoa tai paina "Enter" lopettaaksesi: ')
     if syote == "":
-        print("lopetetaan")
+        print(punainen("lopetetaan"))
         break
-    if syote == "lisaa":
-        lisaa_s = input('''Kirjoita tiedot jotka haluat lisätä muodossa: "icao","iata","name","city","subd","country","elevation","lat","lon","tz","lid"\n''')
-        kirjoita(lisaa_s)
-    if syote == "etsi":
+    elif syote == "lisaa":
+        lisaa_ICAO = input("Kentän ICAO: ")
+        lisaa_IATA = input("Kentän IATA: ")
+        lisaa_NAME = input("Kentän NAME: ")
+        lisaa_CITY = input("Kentän CITY: ")
+        lisaa_SUBD = input("Kentän SUBD: ")
+        lisaa_COUNTRY = input("Kentän COUNTRY: ")
+        lisaa_ELEVATION = input("Kentän ELEVATION: ")
+        lisaa_LAT = input("Kentän LAT: ")
+        lisaa_LON = input("Kentän LON: ")
+        lisaa_TZ = input("Kentän TZ: ")
+        lisaa_LID = input("Kentän LID: ")
+        lisaa_l = [lisaa_ICAO, lisaa_IATA, lisaa_NAME, lisaa_CITY, lisaa_SUBD, lisaa_COUNTRY, lisaa_ELEVATION, lisaa_LAT, lisaa_LON, lisaa_TZ, lisaa_LID]
+        kirjoita(lisaa_l)
+    elif syote == "etsi":
         etsi_s = input("Syötä lentokentän ICAO koodi etsiäksesi tietoa kentästä: ")
         print(etsi_t(etsi_s))
     else:
