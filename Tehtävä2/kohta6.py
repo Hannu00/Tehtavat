7ab78f0da29e4cf529a814616f29f8d862b6d017
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-09-01 13:35:57 +0300

    Lisätty kommentit

--- Tehtävä2/kohta6.py
+++ Tehtävä2/kohta6.py
@@ -1,9 +1,13 @@
 import random
 
+# Kolmi numeroinen koodi. Luodaan tekemällä kaksi eri numeroa jotka yhdistetään jotta koodin ensimmäinen numero voi olla myös 0
 yksi = str(random.randint(0,9))
 kaksi = str(random.randint(10,99))
+kolme = yksi+kaksi
 
+# Neli numeroinen koodi
 nelja = random.randint(1000,6666)
 
+# Tulostetaan koodit
 print(yksi+kaksi)
 print(nelja)
