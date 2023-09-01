155e85ddf3e2c579860aa1157435fddd124ca1ba
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-09-01 13:26:25 +0300

    grammat korjattu 13.3 eikä 13.5

--- Tehtävä2/kohta5.py
+++ Tehtävä2/kohta5.py
@@ -4,7 +4,7 @@ luoti = float(input("Syötä luodit: "))
 
 naula = naula + (leiviska * 20)
 luoti = luoti + (naula * 32)
-paino = float(luoti * 13.5)
+paino = float(luoti * 13.3)
 kilo = int(paino / 1000)
 gramma = paino % 1000
 grammaval = round(gramma, 2)
