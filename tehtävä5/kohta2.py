038b7558abf4672e96dabc0baa6bd58aa91b5fae
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-09-08 13:22:55 +0300

    korjattu while true pois

--- tehtävä5/kohta2.py
+++ tehtävä5/kohta2.py
@@ -2,11 +2,13 @@ luvut = []
 luku = input("Syötä luku ja lopuksi paina Enter lopettaaksesi: ")
 
 while luku != "":
-    x = int(luku)
-    luvut.append(x)
-    luku = input("Syötä luku ja lopuksi paina Enter lopettaaksesi: ")
+        x = int(luku)
+        luvut.append(x)
+        luku = input("Syötä luku ja lopuksi paina Enter lopettaaksesi: ")
 
 luvut.sort(reverse=True)
-
-for i in range(5):
-    print(luvut[i])
+if len(luvut) >= 5:
+    for i in range(5):
+        print(luvut[i])
+else:
+    print("Listassa ei tarpeeksi numeroita!")
