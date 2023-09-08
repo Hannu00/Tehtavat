3ada5779501651903c93e6f6b1e2c6f92467df77
Hannu00 <rasmus.jokelainen@metropolia.fi>
2023-09-08 15:04:59 +0300

    muotoiltu edes jotenkin paremmin

--- Tehtävä6/kohta6.py
+++ Tehtävä6/kohta6.py
@@ -1,18 +1,20 @@
 import math
 
+
+def pizzuri(halkaisija, eurot):
+    a = math.pi * (halkaisija / 2) ** 2
+    b = a / eurot
+    return b
+
+
 pizza_h1 = float(input("Syötä pizzan1 halkaisija senttimetreinä: "))
 pizza_e1 = float(input("Syötä pizzan1 hinta euroina: "))
 
 pizza_h2 = float(input("Syötä pizzan2 halkaisija senttimetreinä: "))
 pizza_e2 = float(input("Syötä pizzan2 hinta euroina: "))
 
-def pizzuri(halkaisija,eurot):
-    a = math.pi * (halkaisija / 2) ** 2
-    b = a / eurot
-    return b
-
-pizza1 = pizzuri(pizza_h1,pizza_e1)
-pizza2 = pizzuri(pizza_h2,pizza_e2)
+pizza1 = pizzuri(pizza_h1, pizza_e1)
+pizza2 = pizzuri(pizza_h2, pizza_e2)
 
 if pizza1 > pizza2:
     print(f"Pizza1 on parempi kuin pizza2")
